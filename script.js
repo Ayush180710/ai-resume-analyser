3627664d9472fd329316aa5c566c80ed9da204bb
Ayush <ayush002ht@gmail.com>
2026-03-14 11:20:53 +0530

    Change API endpoint to production URL

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ async function fetchInsights(resumeText) {
     aiContainer.innerHTML = "<em>Generating insights...</em>";
 
     try {
-        const response = await fetch('http://localhost:3000/api/analyze', {
+        const response = await fetch('https://ai-resume-analyser-eobn.onrender.com', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ resumeText })
